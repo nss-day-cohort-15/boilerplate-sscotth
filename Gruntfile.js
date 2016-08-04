6b238889a30795adeb88ec86c1af663e5a9a8662
Matt Kraatz <matt.kraatz@gmail.com>
2016-08-04 12:08:57 -0500

    added a few linters, added livereload to gruntfile and index

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,24 +4,62 @@ module.exports = function (grunt) {
   // Load grunt plugins
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-stylelint');
+  grunt.loadNpmTasks('grunt-jsonlint');
+  grunt.loadNpmTasks('grunt-htmlhint');
 
   // Plugin configuration
   grunt.initConfig({
     jshint: {
-      files: [
-        './src/**/*.js',
-        '!./src/lib/**/*'
-      ],
-      options: {
-        jshintrc: true
+      all: {
+        src: ['./**/*.js', '!./node_modules/**/*', '!./src/**/*', '!./Gruntfile.js'],
+        options: {
+          jshintrc: true
+        }
+      }
+    },
+    stylelint: {
+      all: {
+        src: ['./**/*.css', '!./node_modules/**/*', '!./src/**/*']
       }
     },
     watch: {
-      files: [
-        './src/**/*.js',
-        '!./src/lib/**/*'
-      ],
-      tasks: ['jshint']
+      options: {
+        livereload: true
+      },
+      js: {
+        files: ['./**/*.js', '!./node_modules/**/*', '!./src/**/*'],
+        tasks: ['jshint']
+      },
+      html: {
+        files: ['./**/*.html', '!./node_modules/**/*', '!./src/**/*'],
+        tasks: ['htmlhint']
+      },
+      css: {
+        files: ['./**/*.css', '!./node_modules/**/*', '!./src/**/*'],
+        tasks: ['stylelint']
+      },
+      json: {
+        files: ['./**/*.json', '!./node_modules/**/*', '!./src/**/*'],
+        tasks: ['jsonlint']
+      }
+    },
+    jsonlint: {
+      all: {
+        src: ['./**/*.json', '!./node_modules/**/*', '!./src/**/*'],
+        options: {
+          format: true,
+          indent: 2
+        }
+      }
+    },
+    htmlhint: {
+      all: {
+        src: ['./**/*.html', '!./node_modules/**/*', '!./src/**/*'],
+        options: {
+          htmlhintrc: '.htmlhintrc'
+        }
+      }
     }
   });
 };
